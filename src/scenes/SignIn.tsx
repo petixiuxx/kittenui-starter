39dd28fe949077a7e8ce95085287bf735bd2b85c
Harry <harry@onlinebizsoft.com>
2020-03-01 02:53:25 +0700

    first commit

---
+++ src/scenes/SignIn.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ContentView from "../layouts/signIn";
+
+export const SignIn1Screen = ({ navigation }): React.ReactElement => (
+  <ContentView navigation={navigation} />
+);
